4c4b33e402559949a3e3f07d0e4d298fe2eccaac
Calvine Akoonyatse <cakoonyatse@gmail.com>
2022-04-23 12:56:27 +0200

    Working random words added to array
    -Fixed "100.txt" from single element to one word per line
    -Working ArrayTest with prints for testing *ONLY*

---
+++ ArrayTest.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayTest {
+    public static void main(String[] args) throws IOException {
+        List<String> wordsList = Files.readAllLines(Paths.get("100.txt"));
+        Collections.shuffle(wordsList);
+        List<String> random20WordsList = wordsList.subList(0, 1 + 20);
+        System.out.printf("random20WordsList = %s%n", random20WordsList);
+        String[] random20WordsArray = random20WordsList.toArray(new String[20]);
+        System.out.printf("random20WordsArray = %s%n", Arrays.toString(random20WordsArray));
+    }
+}
