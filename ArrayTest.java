49c235a1c4979d55c611fd97b4fcffeaecb62dfd
Calvine Akoonyatse <cakoonyatse@gmail.com>
2022-04-24 14:31:07 +0200

    Bubble Sort
    Added test bubble sort class to use *After adding the 20 words to the array with ArrayTest*
    --Unfinished "c. A sorted list of words will be saved to a file, and displayed to the user to view"

--- ArrayTest.java
+++ ArrayTest.java
@@ -9,7 +9,7 @@ public class ArrayTest {
     public static void main(String[] args) throws IOException {
         List<String> wordsList = Files.readAllLines(Paths.get("100.txt"));
         Collections.shuffle(wordsList);
-        List<String> random20WordsList = wordsList.subList(0, 1 + 20);
+        List<String> random20WordsList = wordsList.subList(0, 19);
         System.out.printf("random20WordsList = %s%n", random20WordsList);
         String[] random20WordsArray = random20WordsList.toArray(new String[20]);
         System.out.printf("random20WordsArray = %s%n", Arrays.toString(random20WordsArray));
