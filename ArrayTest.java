b740abd1e7672569f452864f083958700793df73
Calvine Akoonyatse <cakoonyatse@gmail.com>
2022-05-21 22:05:09 +0200

    Update ArrayTest.java

--- ArrayTest.java
+++ ArrayTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 
 public class ArrayTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<String> wordsList = Files.readAllLines(Paths.get("100.txt"));
         Collections.shuffle(wordsList);
         List<String> random20WordsList = wordsList.subList(0, 19);
