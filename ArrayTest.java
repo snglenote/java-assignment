a51f9af15d8a3f191923d6707b91a9824a26bc7b
Calvine Akoonyatse <cakoonyatse@gmail.com>
2022-05-23 12:39:18 +0200

    Update ArrayTest.java

--- ArrayTest.java
+++ ArrayTest.java
@@ -11,40 +11,13 @@ public class ArrayTest {
     public static void main(String[] args) throws IOException {
         List<String> wordsList = Files.readAllLines(Paths.get("100.txt"));
         Collections.shuffle(wordsList);
-        List<String> random20WordsList = wordsList.subList(0, 19);
+        List<String> random20WordsList = wordsList.subList(0, 20);
         System.out.printf("random20WordsList = %s%n", random20WordsList);
         String[] random20WordsArray = random20WordsList.toArray(new String[20]);
         System.out.printf("random20WordsArray = %s%n", Arrays.toString(random20WordsArray));
     }
   
-    static long startTime = System.currentTimeMillis();
-    
-        public static void bubbleSort() {
-
-         String str[] = {"Lorem" , "Ipsum" , "Dolor" , "Sit" , "Etiam" ,  "Amet" , "Consectetur", "Adipiscing" , "Elit" , "Integer",  "Auctor", "Purus", "Nec" , "Congue", "Dolor" , "Tellus" , "Scelerisque" , "Mongo", "Nam", "Diam"};
-
-         String temp;
-            System.out.println("Strings in sorted order:");
-                for (int j = 0; j < str.length; j++) {
-                for (int i = j + 1; i < str.length; i++) {
-                    
-                if (str[i].compareTo(str[j]) < 0) {
-                 temp = str[j];
-                 str[j] = str[i];
-                 str[i] = temp;
-             }
-             
-             long endTime = System.currentTimeMillis();
-
-             System.out.println("That took " + (endTime - startTime) + " milliseconds");
-             
-            }
-             System.out.println(str[j]);
-
-            
-            }            
-        }
-    }
+ }
 
 
 
